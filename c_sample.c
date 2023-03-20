b6801fd9fc8cb5d719da08e66b6645243a9ce79c
Alex Hsu <alex9810171@gmail.com>
2023-03-20 16:20:20 +0000

    first commit

---
+++ c_sample.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void for_function(){
+	int x = 0;
+	for (int i = 1; i <= 5; i++){
+		x += i;		
+	}
+	printf("%d\n", x);
+}
+
+void qq_function(){
+	int x = 100;
+	for (x; x>0; x-=10){
+		printf("QQ\n");
+	}
+}
